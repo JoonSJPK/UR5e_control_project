c86f60150e01f2f0a8d7fc81a1b9ccca89cdf7a8
JoonSJPK <park.p.saejoon@gmail.com>
2026-06-10 17:44:39 -0400

    Updated: trajectories

--- src/task2_trajectory_tracking/trajectories.py
+++ src/task2_trajectory_tracking/trajectories.py
@@ -1,5 +1,8 @@
 import os
 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 import mujoco
 import mujoco.viewer
 from tuned_controllers import PIDController
@@ -12,17 +15,12 @@ def main():
   data = mujoco.MjData(model)
   dt = model.opt.timestep
 
-  fsm_state = 0
-  fsm_pose1 = 0
-  fsm_pose2 = 1
-
-  targets = []
-  targets.append([-1.5708, -1.5708, -1.5708, -1.5708, -1.5708, -1.5708,])
-  targets.append([1.5708, -1.5708, 1.5708, 1.5708, 1.5708, 1.5708,])
+  init  = [-1.5708, -1.5708, -1.5708, -1.5708, -1.5708, -1.5708]
+  target = [1.5708, -1.5708, 1.5708, 1.5708, 1.5708, 1.5708]
 
   controllers = []
   for idx in range(6):
-     controllers.append(PIDController(joint = idx))
+     controllers.append(PIDController(joint = idx, target = target[idx], init = init[idx]))
 
   prev_time = 0.0
 
@@ -30,6 +28,12 @@ def main():
   #trans_state = [0, 1, 2]
 
   #integrate trapazoid velocity equation to get positions
+
+  #variable setup
+  steps_total = int(10 / dt)
+  collect = {idx: [] for idx in range(6)}
+  count = 0
+  plot_saved = False
 
   with mujoco.viewer.launch_passive(model, data) as viewer:
         while viewer.is_running():
@@ -41,25 +45,74 @@ def main():
             prev_time = data.time
             
             #apply torque to each joint
-            if( data.time <= trans_time ):
-              for idx, controller in enumerate(controllers):
-                  data.qfrc_applied[idx] = controller.compute(
-                      dt, targets[0][idx], data.qpos[idx], data.qvel[idx]
-                  )
+            #if( data.time <= trans_time ):
+              #for idx, controller in enumerate(controllers):
+                 # data.qfrc_applied[idx] = controller.compute(
+                     # dt, targets[0][idx], data.qpos[idx], data.qvel[idx]
+                 # )
 
             #apply torque to each joint
-            if( data.time > trans_time ):
-              for idx, controller in enumerate(controllers):
-                  data.qfrc_applied[idx] = controller.compute(
-                      dt, targets[1][idx], data.qpos[idx], data.qvel[idx]
-                  )
+            #if( data.time > trans_time ):
+             # for idx, controller in enumerate(controllers):
+            #      data.qfrc_applied[idx] = controller.compute(
+             #         dt, targets[1][idx], data.qpos[idx], data.qvel[idx]
+             #     )
+
+            if( data.time >= 0.0 and data.time < trans_time ):
+                for idx, controller in enumerate(controllers):
+                    data.qfrc_applied[idx] = controllers[idx].compute(
+                        dt, init[idx], data.qpos[idx], data.qvel[idx]
+                    )
+            elif( data.time >= trans_time and data.time <= trans_time + 4):
+                trans_tgt = []
+                for idx, controller in enumerate(controllers):
+                    curr_time = data.time - trans_time
+                    trans_tgt.append(controllers[idx].compute_tgt_pos(curr_time, data.qpos[idx]))
+
+                for idx, controller in enumerate(controllers):
+                    tgt_vel = controllers[idx].compute_tgt_vel(curr_time)
+                    data.qfrc_applied[idx] = controller.compute(
+                        dt, trans_tgt[idx], data.qpos[idx], data.qvel[idx], tgt_vel
+                    )
+                #collect data
+                if count < steps_total:
+                    for idx in collect:
+                        collect[idx].append(float(data.qvel[idx]))
+                    count += 1
+            else:
+                for idx, controller in enumerate(controllers):
+                    data.qfrc_applied[idx] = controllers[idx].compute(
+                        dt, target[idx], data.qpos[idx], data.qvel[idx]
+                    )
+                #collect data
+                if count < steps_total:
+                    for idx in collect:
+                        collect[idx].append(float(data.qvel[idx]))
+                    count += 1
 
             
 
-
+            
             #update simulation
             mujoco.mj_step(model, data)
             viewer.sync()
+
+
+
+            if count == steps_total and not plot_saved:
+                times = [i * dt for i in range(len(collect[4]))]
+                fig, ax = plt.subplots()
+                ax.plot(times, collect[4])
+                ax.set_xlabel("Time (s)")
+                ax.set_ylabel("Velocity (rad/s)")
+                ax.set_title("Joint 4 Velocity")
+                out_path = os.path.join(os.path.dirname(__file__), "vel5_graph.png")
+                fig.savefig(out_path)
+                plt.close(fig)
+                print(f"Plot saved to {out_path}")
+                plot_saved = True
+
+                
 
 
 
--- src/task2_trajectory_tracking/tuned_controllers.py
+++ src/task2_trajectory_tracking/tuned_controllers.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 class PIDController:
 
-  def __init__(self, joint):
+  def __init__(self, joint, target, init):
 
     match joint:
       case 0:
@@ -39,13 +39,51 @@ class PIDController:
           self.Kd = 0.0
     
     self.integral = 0.0
+    self.t1 = 1
+    self.t2 = 3
+    self.t3 = 4
+    
+    self.target = target
+    self.init = init
+    self.a = (target - init) / 3
+    self.b = self.a
+    self.c = self.a * self.t3
 
   def reset(self):
     self.integral = 0.0
 
-  def compute(self, dt, target_qpos, curr_qpos, curr_qvel):
+  def compute(self, dt, target_qpos, curr_qpos, curr_qvel, target_qvel=0.0):
     error = target_qpos - curr_qpos
     self.integral += error * dt
-    torques = (self.Kp * error) + (self.Ki * self.integral) - (self.Kd * curr_qvel)
+    torques = (self.Kp * error) + (self.Ki * self.integral) + (self.Kd * (target_qvel - curr_qvel))
 
     return torques
+
+  def compute_tgt_vel(self, t):
+      if t >= 0 and t < self.t1:
+          return self.a * t
+      elif t >= self.t1 and t < self.t2:
+          return self.a
+      elif t >= self.t2 and t < self.t3:
+          return self.a * (self.t3 - t)
+      else:
+          return 0.0
+  
+  def compute_tgt_pos(self, t, curr_pos):
+      if( t >= 0 and t < self.t1 ):
+        tgt_pos = ((self.a * t * t) / 2) + self.init
+
+        return tgt_pos
+
+      elif( t >= self.t1 and t < self.t2 ):
+        tgt_pos = (self.b * t) + ((self.a * self.t1 * self.t1) / 2) + (self.init) - (self.b * self.t1)
+
+        return tgt_pos
+
+      elif( t >= self.t2 and t < self.t3 ):
+        tgt_pos = -((self.a * t * t) / 2) + (self.c * t) + (self.b * self.t2) + ((self.a * self.t1 * self.t1) / 2) + (self.init) - (self.b * self.t1) + ((self.a * self.t2 * self.t2) / 2) - (self.c * self.t2)
+
+        return tgt_pos
+      
+      #else:
+         #return curr_pos
